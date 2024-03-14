562f9c7ffd921d11d2b89cdb3b8ccc94017b9fc2
Ole-Kristian <162562626+Ole-Kristian@users.noreply.github.com>
2024-03-14 11:38:02 +0100

    Har lagd navbar

--- prosjektarbeid/java.js
+++
@@ -1,9 +0,0 @@
-/* const burgerEl = document.querySelector('.fa-bars')
-
-const navEl =document.querySelector('nav')
-
-burgerEl.addEventListener('click', showNav)
-
-function showNav(){
-    navEl.classList.toggle('show')
-} */
