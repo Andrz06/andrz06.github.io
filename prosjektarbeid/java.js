dc85902f96e50c72e5a2e6e1f104c406fcd1fb3f
Ole-Kristian <162562626+Ole-Kristian@users.noreply.github.com>
2024-03-19 14:48:04 +0100

    endret sammen

--- prosjektarbeid/java.js
+++ prosjektarbeid/java.js
@@ -1,2 +1,10 @@
 let spillEl = document.querySelector ('#spill')
 
+let divEl = document.createElement('p')
+
+for(i=0; i<6; i++){
+    spillEl.appendChild(divEl)
+    divEl.innerHTML = "hei på deg"
+}
+
+
