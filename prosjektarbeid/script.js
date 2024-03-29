e387e84fcef2807ac23a320723565bab6b0d8d70
Ole-Kristian <162562626+Ole-Kristian@users.noreply.github.com>
2024-03-29 19:28:40 +0100

    prøvde å legge til lenke på hvert div-element, men fikk bare til å få det på teksten

--- prosjektarbeid/script.js
+++ prosjektarbeid/script.js
@@ -5,39 +5,47 @@ let spillEl = document.querySelector('#spill')
 let boks1 = {
     navn: "Flappy",
     bilde: "bilder_index/flappy-bird.jpg",
+    lenke: "flappybird/Flappybird.html"
 }
 let boks2 = {
     navn: "Tre på rad",
     bilde: "bilder_index/fugl.jpeg",
+    lenke: "flappybird/Flappybird.html"
 }
 let boks3 = {
     navn: "Egg i kurv",
     bilde: "bilder_index/fugl.jpeg",
+    lenke: "flappybird/Flappybird.html"
 }
 let boks4 = {
     navn: "fortnite",
     bilde: "bilder_index/fugl.jpeg",
+    lenke: "flappybird/Flappybird.html"
 }
 let boks5 = {
     navn: "fortnite",
     bilde: "bilder_index/fugl.jpeg",
+    lenke: "flappybird/flappytest.html"
 }
 
 let spillArr = [boks1, boks2, boks3, boks4, boks5]
 
-for(i=0;i<spillArr.length;i++){
+for(i=0; i<spillArr.length; i++){
     let divEl = document.createElement('div');
     spillEl.appendChild(divEl);
 
+    
+
     let navnEl = document.createElement('a');
-    //let bildeEl = document.createElement('img')
-    //bildeEl.src = spillArr[i].bilde;
     divEl.style.backgroundImage = `url(${spillArr[i].bilde})`
     navnEl.innerHTML = `${spillArr[i].navn}`;
     divEl.appendChild(navnEl);
-    //divEl.appendChild(bildeEl)
+    navnEl.href = spillArr[i].lenke
+
 
 }
+
+
 
 
 
