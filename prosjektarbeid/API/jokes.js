f704474c28295f5ea73d674d5b290903c8f6568b
Ole-Kristian <162562626+Ole-Kristian@users.noreply.github.com>
2024-04-15 08:50:46 +0200

    endringer

--- prosjektarbeid/API/jokes.js
+++ prosjektarbeid/API/jokes.js
@@ -1,5 +1,24 @@
 let jokeBtn = document.querySelector('button')
+let jokeEl = document.querySelector ('#joke')
 
-jokeBtn.addEventListener ('click', async function(){
-let url ="https://api.api-ninjas.com/v1/randomimage?category="
-})
+jokeBtn.addEventListener ('click', jokegenerator)
+
+
+async function jokegenerator (){
+
+let url = "https://api.api-ninjas.com/v1/jokes?limit="
+
+let config = {
+    headers: {
+        'Accept' : 'application/json'
+}
+}
+
+let response = await fetch (url, config)
+
+let data = await response.json()
+
+console.log(data)
+
+jokeEl.innerHTML = data["joke"]
+}
