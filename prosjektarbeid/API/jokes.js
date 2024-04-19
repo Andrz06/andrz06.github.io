9b9d60838452e9ca76c85727c28d5a48c07315d6
Ole-Kristian <162562626+Ole-Kristian@users.noreply.github.com>
2024-04-19 10:07:36 +0200

    fikset knapper

--- prosjektarbeid/API/jokes.js
+++ prosjektarbeid/API/jokes.js
@@ -1,23 +1,23 @@
-let Btn = document.querySelector('button')
+let Btn = document.querySelector('#button')
 let statementEl = document.querySelector ('#statement')
-let rightEl = document.querySelector ('#right')
-let wrongEl = document.querySelector ('#wrong')
+let rightEl = document.querySelector ('#true')
+let wrongEl = document.querySelector ('#false')
 
 
-Btn.addEventListener ('click', jokegenerator)
+Btn.addEventListener ('click', quizgenerator)
 
 
-async function jokegenerator (){
+async function quizgenerator (){
 
 i=1
 console.log("hei")
 
-let url = "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean"
+let url = "https://opentdb.com/api.php?amount=15&category=27&difficulty=easy&type=boolean"
 
-let config = {
+ let config = {
     headers: {
         'Accept' : 'application/json'
-}
+} 
 }
 
 let response = await fetch(url, config)
@@ -26,11 +26,19 @@ let data = await response.json()
 
 console.log(data)
 
-statementEl.innerHTML = data.results[i]["question"]
+statementEl.innerHTML = `Statement: ${data.results[i]["question"]}`
+
+/* statementEl.innerHTML = data.results[i]["question"]
 rightEl.innerHTML = data.results[i]["correct_answer"]
-wrongEl.innerHTML = data.results[i].incorrect_answers[0]
+wrongEl.innerHTML = data.results[i].incorrect_answers[0] */
 i+=1
 
-
-
 }
+
+score = 0
+
+
+
+
+
+
